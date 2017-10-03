d7608e1e54378baad39de6f1b2715808a8562aac
1412145 <hainguyen1196@gmail.com>
2017-10-03 06:00:53 -0700

    Add view and controller for destroy post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,14 @@ class PostsController < ApplicationController
         redirect_to @post
     end
 
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        
+        redirect_to posts_path
+    end
+
     def edit
         @post = Post.find(params[:id])
     end
