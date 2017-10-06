60ce9beb61f57affb4cf71b4f1a7020b4e7d6507
Hien Ho <1412169@student.hcmus.edu.vn>
2017-10-06 15:30:22 +0700

    create view for home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+  	@photos = Photo.order(created_at: :desc).limit(8) 
+  	@posts = Post.order(created_at: :desc).all.limit(5)
   end
+
+
 end
