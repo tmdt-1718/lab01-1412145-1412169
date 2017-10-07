856d495b85fb952649673b523da7c6bac2a3a383
Hien Ho <1412169@student.hcmus.edu.vn>
2017-10-07 18:41:31 +0700

    change routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+
 Rails.application.routes.draw do
   # get 'home/index'
+
+  get 'posts/:id/viewcount', to: 'posts#getviewcount'
 
   get '/about', to: 'about#show'
   get '/login', to: 'about#login'
@@ -9,8 +12,22 @@ Rails.application.routes.draw do
   resources :posts
   resources :home
   resources :albums do
-  	resources :photos
+    resources :photos
   end
 
-  root 'home#index'
-end
+
+  get '/signup', to: 'accounts#new'
+
+  post'/signup', to: 'accounts#create'
+
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+  
+  delete '/logout', to: 'sessions#destroy'
+
+
+  resources :accounts
+
+
+root 'home#index'
