dc07a3affc5478f7426917bbbd64b7cd2cf58ebc
Hien Ho <1412169@student.hcmus.edu.vn>
2017-10-07 09:27:03 +0700

    format albums and add path for navbar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	@photos = Photo.order(created_at: :desc).limit(4) 
-  	@posts = Post.order(created_at: :desc).all.limit(5)
+  	@photos = Photo.order(created_at: :desc).limit(6) 
+  	@posts = Post.order(created_at: :desc).all.limit(8)
   end
-
-
 end
